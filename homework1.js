19060015d09835fbe067c074a16ec4564ff7abfe
kosyananya <45904651+kosyananya@users.noreply.github.com>
2021-06-12 13:06:11 +0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,69 @@
+ // problem 1
+    // let variableOne = +prompt()
+    // let variableTwo = +prompt()
+    // variableOne = variableTwo - variableOne
+    // variableTwo = variableTwo - variableOne
+    // variableOne = variableTwo + variableOne
+    // alert("variableOne = " + variableOne + " variableTwo =" + variableTwo)
+
+
+    //problem 2
+    // const num = +prompt()
+    // const lastDigit = num % 10
+    // if (num === lastDigit || !lastDigit) {
+    //     alert(num)
+    // } else {
+    //     alert(Number(String(lastDigit) + Math.floor(num / 10)))
+    // }
+
+
+
+    //problem 3
+    // const num1 = +prompt()
+    // const num2 = +prompt()
+    // const num3 = +prompt()
+    // const num4 = +prompt()
+    // const num5 = +prompt()
+    // alert((num1 + num2 + num3 + num4 + num5) / 5)
+
+    // problem 4
+    // const num1 = +prompt()
+    // const num2 = +prompt()
+    // const num3 = +prompt()
+    // if (num1 < num2 && num1 < num3) {
+    //     if (num2 < num3) {
+    //         console.log(num1 + ", " + num2 + ", " + num3);
+    //     }
+    //     else {
+    //         console.log(num1 + ", " + num3 + ", " + num2);
+    //     }
+    // }
+    // else if (num2 < num1 && num2 < num3) {
+    //     if (num1 < num3) {
+    //         console.log(num2 + ", " + num1 + ", " + num3);
+    //     }
+    //     else {
+    //         console.log(num2 + ", " + num3 + ", " + num1);
+    //     }
+    // }
+    // else if (num3 < num1 && num3 < num2) {
+    //     if (num1 < num2) {
+    //         console.log(num3 + ", " + num1 + ", " + num2);
+    //     }
+    //     else {
+    //         console.log(num3 + ", " + num2 + ", " + num1);
+    //     }
+    // }
+
+    // problem 5
+    // let n = +prompt()
+    // let i = 0
+    // let j = 0
+
+    // if (!(n % 2) && !Math.floor(n / 10)) {
+    //     i += 1
+    // }
+    // if (!(n % 3) && n % 10 === 1) {
+    //     j += 1
+    // }
+    // console.log(i, j)
