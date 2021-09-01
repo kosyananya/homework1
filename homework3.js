ce3c6ecb092f20440287e2f39f8001d777ec2d85
kosyananya <45904651+kosyananya@users.noreply.github.com>
2021-09-02 00:00:05 +0400

    Add files via upload

--- homework3.js
+++ homework3.js
@@ -1,73 +1,76 @@
-// propbel 1
+//problem 1
 
-function createArray(start, end, space) {
-    let arr = []
-    let n = start
-    if (space > 0) {
-        while (n <= end) {
-            arr.push(n)
-            n = n + space
-        }
+function customShift(arr, i = 0) {
+    if(arr.length === 0) {
+        return arr
+    } else if(arr.length === 1) {
+        arr.length = 0 
+        return arr
+    }else if(i === arr.length - 1) {
+        arr.length = arr.length - 1
         return arr
     } else {
-        return "It is impossible to do"
+        let temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+        return customShift(arr, i + 1)
     }
 }
-createArray(1, 17, 4)
+customShift([1,2,3,4,5,6])
+
+//problem 2
 
 
-// problem 2
-
-function maxElement(arr) {
-    let max = arr[0]
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i]
-        }
+function flatten(arr, flatArr) {
+    if (arr.length === 0) {
+      return flatArr
     }
-    return max
+    let firstElem = arr[0]
+    let rest = arr.slice(1)
+    if (Array.isArray(firstElem)) {
+      return flatten(firstElem.concat(rest), flatArr)
+    } else {
+      flatArr.push(firstElem)
+      return flatten(rest, flatArr)
+    }
+    
+  }
+
+
+//problem 3
+
+function calculateSum(num) {
+    num = num.toString().split('')
+    num = num.reduce(function fnSum(a,b) {
+        return +a + +b
+    }, 0)
+
+    if(num < 10) {
+        return num
+    }else {
+        return calculateSum(num)
+    }
+
 }
-maxElement([1, 2, 0, -9, 10, 8])
 
+//problem 4 
 
-// problem 3
-
-function reverseArray(arr) {
-    let temp
-    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-        temp = arr[i]
-        arr[i] = arr[arr.length - i - 1]
-        arr[arr.length - i - 1] = temp
+function invertKeyValue(obj) {
+  let result = {}
+  for(let key in obj) {
+    if (obj[key] in result) {
+      if(Array.isArray(result[obj[key]])) {
+        result[obj[key]].push(key)
+      } else {
+        result[obj[key]] = [result[obj[key]], key]
+      }
+    } else {
+      result[obj[key]] = key
     }
-    return arr
+  }
+  return result
 }
-reverseArray([1, 2, 3, 4, 5])
 
-// problem 4
-
-function productsTwoNeighbours(arr) {
-    let newArr = []
-    for (let i = 0; i< arr.length - 1; i++) {
-        newArr.push(arr[i] * arr[i + 1])
-    }
-    return newArr
-}
-productsTwoNeighbours([1,2,3,4])
+//problem 5
 
 
-// problem 5
-
-function getNewArr(arr, num) {
-    let isContein = false
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === num) {
-            arr.splice(i, 1)
-            isContein = true
-        }
-    }
-    if (!isContein) {
-        arr.push(num)
-    }
-    return arr
-}
-getNewArr([1, 2, 3], 4)
